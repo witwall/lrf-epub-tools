01d18c27f0b9b351c7cb8c3fe6591998c2d963ce
eladio.linares <eladio.linares@f2d9b3e6-a1e5-11dd-a648-ff032ea17e63>
2009-03-04 18:40:32 +0000

    OpenXML (.docx Word 2007) to EPUB Conversion added

--- trunk/lrf-epub-tools/src/lrf/docx/states/STMain.java
+++ trunk/lrf-epub-tools/src/lrf/docx/states/STMain.java
@@ -2,7 +2,6 @@ package lrf.docx.states;
 
 import java.util.ArrayList;
 
-import lrf.conv.BaseRenderer;
 import lrf.docx.Context;
 import lrf.docx.SHDocument;
 import lrf.docx.SHRelations;
@@ -12,7 +11,7 @@ import org.xml.sax.Attributes;
 public class STMain implements State {
 	private ArrayList<String> element;// temporary localName list
 	private static STMain singleton = new STMain();
-	private final int FONTSZ = 7;
+	private final int FONTSZ = 1;
 
 	private STMain() {
 		element = new ArrayList<String>();
@@ -23,46 +22,48 @@ public class STMain implements State {
 	}
 
 	public void startDoc(Context context) {
-		String str = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-				+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" "
-				+ "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
-				+ "<html xmlns=\"http://www.w3.org/1999/xhtml\">" + "<head>"
-				+ "<meta http-equiv=\"Content-Type\" content=\"text/html;\" />"
-				+ "<title></title>" + "</head>" + "<body>";
-		context.addData(str);
 	}
 
-	public void startEle(Context context, SHRelations rels, String uri,
-			String lName, String qName, Attributes attributes) {
+	public int lastMarkedDIV=0;
+	public void startEle(Context c, SHRelations rels, String uri,
+			String lName, String qName, Attributes at) {
 		if (!uri.equals(SHDocument.WORDPROCESSINGML))
 			return;
-		if (lName.equals("jc")
-				&& !attributes.getValue(uri, "val").equals("left")) {
-			context.addData("<div style=\"text-align:"
-					+ attributes.getValue(uri, "val") + ";\"> ");
+		if (lName.equals("jc") && !at.getValue(uri, "val").equals("left")) {
+			c.addData("<div style=\"text-align:"+ at.getValue(uri, "val") + ";\">");
 			element.add(lName);
 		} else if (lName.equals("r")) {
 			element.add(lName);
-		} else if (lName.equals("rPr") && element.contains("r")) {
-			element.add(lName);
+		} else if (lName.equals("rPr") ) {
+			if(element.contains("r")){
+				element.add(lName);
+			}
+			if(!element.contains("pStyle"))
+				lastMarkedDIV=c.getEmitLineCount();
 		} else if (lName.equals("color") && element.contains("rPr")) {
 			element.add(lName);
-			context.addData("<font color=\"#" + attributes.getValue(uri, "val")
-					+ "\">");
+			c.addData("<span style=\"font-color=\"#" + at.getValue(uri, "val")+ "\">");
 		} else if (lName.equals("sz") && element.contains("rPr")) {
 			element.add(lName);
-			int fontsize = Integer.parseInt(attributes.getValue(uri, "val"))
-					/ FONTSZ;
-			context.addData("<font size=\"" + fontsize + "\">");
+			int fontsz = Integer.parseInt(at.getValue(uri, "val")) / FONTSZ;
+			c.addData("<span style=\"font-size:" + fontsz + "\">");
 		} else if(lName.equals("lastRenderedPageBreak")){
-			context.addPageBreak();
+			c.addPageBreak();
 		} else if(lName.equals("instrText")){
-			context.avoidCharsEmits=true;
+			c.avoidCharsEmits=true;
+		} else if(lName.equals("pStyle")){
+			c.addData("<div class=\""+at.getValue(uri,"val")+"\">");
+			element.add(lName);
+		} else if(lName.equals("t")){
+			if(!element.contains("pStyle") && c.getStyles().getDefaultStyleName()!=null){
+				c.addDataAt("<div class=\""+c.getStyles().getDefaultStyleName()+"\">",lastMarkedDIV);
+				element.add("pStyle");
+			}
+				
 		}
 	}
 
-	public void endEle(Context context, String uri, String lName,
-			String qName) {
+	public void endEle(Context context, String uri, String lName,String qName) {
 		if (!uri.equals(SHDocument.WORDPROCESSINGML))
 			return;
 		if (lName.equals("pPr")) {
@@ -73,27 +74,29 @@ public class STMain implements State {
 			}
 		} else if (lName.equals("r")) {
 			if (element.contains("rPr") && element.contains("color")) {
-				context.addData("</font>");
+				context.addData("</span>");
 			}
 			if (element.contains("rPr") && element.contains("sz")) {
-				context.addData("</font>");
+				context.addData("</span>");
 			}
 		} else if (lName.equals("p")) {
+			if(element.contains("pStyle")){
+				context.addData("</div>\n");
+				element.remove("pStyle");
+			}
 			if (element.contains("jc")) {
-				context.addData("</div>");
-				element.remove(element.indexOf("jc"));
+				context.addData("</div>\n");
+				element.remove("jc");
 			} else {
 				context.addData("<br/>");
 			}
 			initList();
 		} else if(lName.equals("instrText")){
 			context.avoidCharsEmits=false;
-		}
+		} 
 	}
 
 	public void endDoc(Context context) {
-		context.addData("</body></html>");
-		context.splitOutput();
 	}
 
 	public void initList() {
